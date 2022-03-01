78aff8eb2b6aa09a480f781ae0e6723ecd1bc019
Rohit Kamat <rohit.kamat.itscient@gmail.com>
2022-03-01 16:14:10 +0530

    created components

--- src/app/home-service.service.ts
+++ src/app/home-service.service.ts
@@ -5,5 +5,22 @@ import { Injectable } from '@angular/core';
 })
 export class HomeServiceService {
 
+  students = [
+    {id: 101, name: "Ram", discription: "Ram Details", email: "Ram@xyz.com"},
+    {id: 102, name: "Shyam", discription: "Shyam Details", email: "Shyam@xyz.com"},
+    {id: 103, name: "Abram", discription: "Abram Details", email: "Abram@xyz.com"},
+    {id: 104, name: "Dharam", discription: "Dharam Details", email: "Dharam@xyz.com"}
+  ]
   constructor() { }
+
+  /**
+   * name
+   */
+  public getStudents(): Array<{id: any, name: any, discription: any, email: any}> {
+    return this.students
+  }
+  
+  public createStudent(students: {id: any, name: any, discription: any, email: any}) {
+    this.students.push(students);
+  }
 }
