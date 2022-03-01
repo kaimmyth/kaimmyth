b10c0b6f32eaadcc41163501bd5fc379e7f45f41
Rohit Kamat <rohit.kamat.itscient@gmail.com>
2022-03-01 10:10:42 +0530

    defined class and component changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Name } from "../home";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  myname: Name={
+    name: 'Edureka!'
+  }
+
 
   constructor() { }
 
